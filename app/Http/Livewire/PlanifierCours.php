6a3cd8b5cc7e7dab19f9d6497bda4337986d2ed4
adams <adamoun505@gmail.com>
2022-05-24 14:54:05 +0100

    auhomatisation de la sélection des salles ues horaires, jours et enseignants

--- app/Http/Livewire/PlanifierCours.php
+++ app/Http/Livewire/PlanifierCours.php
@@ -15,15 +15,70 @@ class PlanifierCours extends Component
     public $niveaux;
     public $ues;
     public $niv;
-    public $matiere;
+    public $matiere1;
     public $eff;
     public $mesues;
+    public $mesues2;
+    public $matiere2;
+    public $mesues3;
+    public $matiere3;
+    public $matiere4;
+    public $mesues4;
+    public $salles1;
+    public $jour1;
+    public $horaire1;
+    public $id_salle;
+    public $enseignants1;
+    public $id_ens;
+    public $id_ense;
 
-    public function updatedMatiere(){
-        $this->mesues = DB::table('ues')->where('id_niveau', $this->eff[0]['id'])->where('nom_ue', '<>', $this->matiere)->get();
+    public function updatedMatiere3(){
+        //recupérer la liste des ues qui n'ont pas encore essayé
+        $this->mesues4 = DB::table('ues')->where('id_niveau', $this->eff[0]['id'])->where('nom_ue', '<>', $this->matiere2)->where('nom_ue', '<>', $this->matiere1)->where('nom_ue', '<>', $this->matiere3)->get();
     }
 
-    public function mount(Request $request){
+    public function updatedMatiere2(){
+        //recupérer la liste des ues qui n'ont pas encore essayé
+        $this->mesues3 = DB::table('ues')->where('id_niveau', $this->eff[0]['id'])->where('nom_ue', '<>', $this->matiere2)->where('nom_ue', '<>', $this->matiere1)->get();
+        // dd($this->mesues3);
+    }
+
+    public function updatedMatiere1(){
+        //recupérer la liste des ues qui n'ont pas encore essayé
+        $this->mesues2 = DB::table('ues')->where('id_niveau', $this->eff[0]['id'])->where('nom_ue', '<>', $this->matiere1)->get();
+        // dd($this->mesues2);
+    }
+
+    public function updatedHoraire1(){
+        //recupère l'identifiant d
+        $this->id_salle = DB::table('cours')->where('jour',  $this->jour1)->where('heure_debut', $this->horaire1)->value('id_salle');
+        $this->id_ense = DB::table('cours')->where('jour', $this->jour1)->where('heure_debut', $this->horaire1)->where('id_salle',$this->id_salle)->value('id_ense');
+        if(!empty($this->id_salle)){
+            $this->salles1 = DB::table('salles')->where('id','<>', $this->id_salle)->get();
+        }
+        if(!empty($this->id_ense)){
+            $this->id_ens = DB::table('enseignements')->where('id', $this->id_ense)->value('id_ens');
+            $this->enseignants1 = DB::table('enseignants')->where('id', '<>', $this->id_ens)->get();
+        }
+    }
+
+    public function updatedJour1(){
+         
+        $this->id_salle = DB::table('cours')->where('jour', $this->jour1)->where('heure_debut', $this->horaire1)->value('id_salle');
+        if(!empty($this->id_salle)){
+            $this->salles = DB::table('salles')->where('id','<>', $this->id_salle)->get();
+            dd($this->salles[0]);
+        }
+        
+    }
+
+    public function mount(Request $request)
+    {
+        $this->id_salle = DB::table('cours')->where('jour', $this->jour1)->where('heure_debut', $this->horaire1)->value('id_salle');
+        if(!empty($this->id_salle)){
+            $this->salles = DB::table('salles')->where('id','<>', $this->id_salle)->get();
+        }
+        $this->salles = DB::table('salles')->get();
         $this->filiere = $request->filiere;
         $this->niveau = $request->niveau;
         $this->specialites = $request->choixSpec;
@@ -32,11 +87,37 @@ class PlanifierCours extends Component
         $this->eff = DB::table('niveaux')->where('id_spec', $spec[0]->id)->where('niveau', $this->niveau)->get();
         $this->ues = DB::table('ues')->where('id_niveau', $this->eff[0]->id)->get();
         $this->mesues = DB::table('ues')->where('id_niveau', $this->eff[0]->id)->get();
+        // $this->mesues2 = DB::table('ues')->where('id_niveau', $this->eff[0]->id)->where('nom_ue', '<>', $this->matiere1)->get();
         
     }
     public function render()
     {
+        $this->id_salle = DB::table('cours')->where('jour', $this->jour1)->where('heure_debut', $this->horaire1)->value('id_salle');
+        if(!empty($this->id_salle)){
+            $this->salles1 = DB::table('salles')->where('id','<>', $this->id_salle)->get();
+        }
+        else{
+            $this->salles1 = DB::table('salles')->get();
+        }
         
+        $this->id_ense = DB::table('cours')->where('jour', $this->jour1)->where('heure_debut', $this->horaire1)->where('id_salle',$this->id_salle)->value('id_ense');
+        if(!empty($this->id_ense)){
+            $this->id_ens = DB::table('enseignements')->where('id', $this->id_ense)->value('id_ens');
+            $this->enseignants1 = DB::table('enseignants')->where('id', '<>', $this->id_ens)->get();
+        }
+        else{
+            $this->enseignants1 = DB::table('enseignants')->get();
+        }
+        $spec = DB::table('specialites')->where('nom_spec', $this->specialites)->get();
+        $this->eff = DB::table('niveaux')->where('id_spec', $spec[0]->id)->where('niveau', $this->niveau)->get();
+        $this->mesues = DB::table('ues')->where('id_niveau', $this->eff[0]->id)->get();
+        if(!empty($this->mesues2)){
+            $this->mesues2 = DB::table('ues')->where('id_niveau', $this->eff[0]->id)->where('nom_ue', '<>', $this->matiere1)->get();
+
+        }
+        if(!empty($this->mesues3)){
+            $this->mesues3 = DB::table('ues')->where('id_niveau', $this->eff[0]->id)->where('nom_ue', '<>', $this->matiere2)->where('nom_ue', '<>', $this->matiere1)->get();
+        }
         return view('livewire.planifier-cours')->layout('layouts.app2');
     }
 }
--- resources/views/livewire/planifier-cours.blade.php
+++ resources/views/livewire/planifier-cours.blade.php
@@ -6,7 +6,6 @@
                     {{ $filiere }} {{ $specialites }} {{ $niveau }} {{ $effectif }}
                     <input type="hidden" name="filiere" value="{{ $filiere }}">
                     @csrf
-                    @foreach($ues as $ue)
                     <div class="form-group">
                         <label>{{ __("Niveau") }}</label>
                         <div class="input-group mb-3 bg-soft-light input-group-lg rounded-lg">
@@ -16,35 +15,41 @@
                                 </span>
                             </div>
                             
-                                <select wire:model="matiere" name="niv" id="niv"class="form-control bg-soft-light border-light @error('niv') is-invalid @enderror"  required >
+                                <select wire:model="matiere1" name="matiere1" id="matiere1"class="form-control bg-soft-light border-light @error('matiere1') is-invalid @enderror"  required >
                                     <option value="">ue</option> 
                                     @foreach($mesues as $ue)
                                     <option value="{{ $ue->nom_ue }}">{{ $ue->nom_ue }}</option>
                                     @endforeach
                                 </select>
-                                <select wire:model="niv" name="niv" id="niv"class="form-control bg-soft-light border-light @error('niv') is-invalid @enderror"  required >
-                                    <option value="">enseignant</option> 
-                                    <option value="1"></option>
-                                    <option value="2"></option>
-                                    <option value="3"></option>
-                                    <option value="4"></option>
-                                    <option value="5"></option>
+                                <select wire:model="jour1" name="jour1" id="jour1"class="form-control bg-soft-light border-light @error('jour1') is-invalid @enderror"  required >
+                                    <option value="">jour</option>
+                                    <option value="lundi">lundi</option>
+                                    <option value="mardi">mardi</option>
+                                    <option value="mercredi">mercredi</option>
+                                    <option value="jeudi">jeudi</option>
+                                    <option value="vendredi">vendredi</option>
+                                    <option value="samedi">samedi</option>
+                                    <option value="dimanche">dimanche</option>
                                 </select>
-                                <select wire:model="niv" name="niv" id="niv"class="form-control bg-soft-light border-light @error('niv') is-invalid @enderror"  required >
-                                    <option value="">salle</option> 
-                                    <option value="1"></option>
-                                    <option value="2"></option>
-                                    <option value="3"></option>
-                                    <option value="4"></option>
-                                    <option value="5"></option>
+                                <select wire:model="horaire1" name="horaire1" id="horaire1"class="form-control bg-soft-light border-light @error('horaire1') is-invalid @enderror"  required >
+                                    <option value="">horaire</option>
+                                    <option value="7H">7H-9H55</option>
+                                    <option value="10H05">10H05-12H55</option>
+                                    <option value="13H05">13H05-15H55</option>
+                                    <option value="16H05">16H05-18H55</option>
+                                    <option value="19H05">19H05-21H55</option>
                                 </select>
-                                <select wire:model="niv" name="niv" id="niv"class="form-control bg-soft-light border-light @error('niv') is-invalid @enderror" name="niv"  required >
-                                    <option value="">horaire</option> 
-                                    <option value="1"></option>
-                                    <option value="2"></option>
-                                    <option value="3"></option>
-                                    <option value="4"></option>
-                                    <option value="5"></option>
+                                <select wire:model="salle1" name="salle1" id="salle1"class="form-control bg-soft-light border-light @error('salle1') is-invalid @enderror"  required >
+                                    <option value="">salle/capacité</option>
+                                    @foreach($salles1 as $salle)
+                                        <option value="{{ $salle->nom_salle }}">{{ $salle->nom_salle }}: {{ $salle->capacite }}</option>
+                                    @endforeach
+                                </select>
+                                <select wire:model="enseignant1" name="enseignant1" id="enseignant1"class="form-control bg-soft-light border-light @error('enseignant1') is-invalid @enderror"   required >
+                                    <option value="">enseignant</option>
+                                    @foreach($enseignants1 as $enseignant)
+                                        <option value="{{ $enseignant->nom_ens }}">{{ $enseignant->nom_ens }}</option>
+                                    @endforeach
                                 </select>
                             
                             @error('niveau')
@@ -54,15 +59,174 @@
                             @enderror
                         </div>
                     </div>
-                    @endforeach
-                    
-                    
+                    @if(!empty($mesues2))
+                    <div class="form-group">
+                        <label>{{ __("Niveau") }}</label>
+                        <div class="input-group mb-3 bg-soft-light input-group-lg rounded-lg">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text border-light text-muted">
+                                    <i class="ri-mail-line"></i>
+                                </span>
+                            </div>
+                            
+                                <select wire:model="matiere2" name="matiere2" id="matiere2"class="form-control bg-soft-light border-light @error('matiere2') is-invalid @enderror"  required >
+                                    <option value="">ue</option> 
+                                    @foreach($mesues2 as $ue)
+                                    <option value="{{ $ue->nom_ue }}">{{ $ue->nom_ue }}</option>
+                                    @endforeach
+                                </select>
+                                <select wire:model="jour2" name="jour2" id="jour2"class="form-control bg-soft-light border-light @error('jour2') is-invalid @enderror"  required >
+                                    <option value="">jour</option>
+                                    <option value="lundi">lundi</option>
+                                    <option value="mardi">mardi</option>
+                                    <option value="mercredi">mercredi</option>
+                                    <option value="jeudi">jeudi</option>
+                                    <option value="vendredi">vendredi</option>
+                                    <option value="samedi">samedi</option>
+                                    <option value="dimanche">dimanche</option>
+                                </select>
+                                <select wire:model="horaire2" name="horaire2" id="horaire2"class="form-control bg-soft-light border-light @error('horaire2') is-invalid @enderror"  required >
+                                    <option value="">horaire</option>
+                                    <option value="7H">7H-9H55</option>
+                                    <option value="10H05">10H05-12H55</option>
+                                    <option value="13H05">13H05-15H55</option>
+                                    <option value="16H05">16H05-18H55</option>
+                                    <option value="19H05">19H05-21H55</option>
+                                </select>
+                                <select wire:model="salle2" name="salle2" id="salle2"class="form-control bg-soft-light border-light @error('salle2') is-invalid @enderror"  required >
+                                    <option value="">salle/capacité</option>
+                                    @foreach($salles2 as $salle)
+                                        <option value="{{ $salle->nom_salle }}">{{ $salle->nom_salle }}: {{ $salle->capacite }}</option>
+                                    @endforeach
+                                </select>
+                                <select wire:model="enseignant2" name="enseignant2" id="enseignant2"class="form-control bg-soft-light border-light @error('enseignant2') is-invalid @enderror"   required >
+                                    <option value="">enseignant</option>
+                                    @foreach($enseignants2 as $enseignant)
+                                        <option value="{{ $enseignant->nom_ens }}">{{ $enseignant->nom_ens }}</option>
+                                    @endforeach
+                                </select>
+                            
+                            @error('niveau')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
+                        </div>
+                    </div>
+                    @endif
+                    @if(!empty($mesues3))
+                    <div class="form-group">
+                        <label>{{ __("Niveau") }}</label>
+                        <div class="input-group mb-3 bg-soft-light input-group-lg rounded-lg">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text border-light text-muted">
+                                    <i class="ri-mail-line"></i>
+                                </span>
+                            </div>
+                            
+                                <select wire:model="matiere3" name="matiere3" id="matiere3"class="form-control bg-soft-light border-light @error('matiere3') is-invalid @enderror"  required >
+                                    <option value="">ue</option> 
+                                    @foreach($mesues3 as $ue)
+                                    <option value="{{ $ue->nom_ue }}">{{ $ue->nom_ue }}</option>
+                                    @endforeach
+                                </select>
+                                <select wire:model="jour3" name="jour3" id="jour3"class="form-control bg-soft-light border-light @error('jour3') is-invalid @enderror"  required >
+                                    <option value="">jour</option>
+                                    <option value="lundi">lundi</option>
+                                    <option value="mardi">mardi</option>
+                                    <option value="mercredi">mercredi</option>
+                                    <option value="jeudi">jeudi</option>
+                                    <option value="vendredi">vendredi</option>
+                                    <option value="samedi">samedi</option>
+                                    <option value="dimanche">dimanche</option>
+                                </select>
+                                <select wire:model="horaire3" name="horaire3" id="horaire3"class="form-control bg-soft-light border-light @error('horaire3') is-invalid @enderror"  required >
+                                    <option value="">horaire</option>
+                                    <option value="7H">7H-9H55</option>
+                                    <option value="10H05">10H05-12H55</option>
+                                    <option value="13H05">13H05-15H55</option>
+                                    <option value="16H05">16H05-18H55</option>
+                                    <option value="19H05">19H05-21H55</option>
+                                </select>
+                                <select wire:model="salle3" name="salle3" id="salle3"class="form-control bg-soft-light border-light @error('salle3') is-invalid @enderror"  required >
+                                    <option value="">salle/capacité</option>
+                                    @foreach($salles3 as $salle)
+                                        <option value="{{ $salle->nom_salle }}">{{ $salle->nom_salle }}: {{ $salle->capacite }}</option>
+                                    @endforeach
+                                </select>
+                                <select wire:model="enseignant3" name="enseignant3" id="enseignant3"class="form-control bg-soft-light border-light @error('enseignant3') is-invalid @enderror"   required >
+                                    <option value="">enseignant</option>
+                                    @foreach($enseignants3 as $enseignant)
+                                        <option value="{{ $enseignant->nom_ens }}">{{ $enseignant->nom_ens }}</option>
+                                    @endforeach
+                                </select>
+                            
+                            @error('niveau')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
+                        </div>
+                    </div>
+                    @endif
+                    @if(!empty($mesues4))
+                    <div class="form-group">
+                        <label>{{ __("Niveau") }}</label>
+                        <div class="input-group mb-3 bg-soft-light input-group-lg rounded-lg">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text border-light text-muted">
+                                    <i class="ri-mail-line"></i>
+                                </span>
+                            </div>
+                            
+                                <select wire:model="matiere4" name="matiere4" id="matiere4"class="form-control bg-soft-light border-light @error('matiere4') is-invalid @enderror"  required >
+                                    <option value="">ue</option> 
+                                    @foreach($mesues4 as $ue)
+                                    <option value="{{ $ue->nom_ue }}">{{ $ue->nom_ue }}</option>
+                                    @endforeach
+                                </select>
+                                <select wire:model="jour4" name="jour4" id="jour4"class="form-control bg-soft-light border-light @error('jour4') is-invalid @enderror"  required >
+                                    <option value="">jour</option>
+                                    <option value="lundi">lundi</option>
+                                    <option value="mardi">mardi</option>
+                                    <option value="mercredi">mercredi</option>
+                                    <option value="jeudi">jeudi</option>
+                                    <option value="vendredi">vendredi</option>
+                                    <option value="samedi">samedi</option>
+                                    <option value="dimanche">dimanche</option>
+                                </select>
+                                <select wire:model="horaire4" name="horaire4" id="horaire4"class="form-control bg-soft-light border-light @error('horaire4') is-invalid @enderror"  required >
+                                    <option value="">horaire</option>
+                                    <option value="7H">7H-9H55</option>
+                                    <option value="10H05">10H05-12H55</option>
+                                    <option value="13H05">13H05-15H55</option>
+                                    <option value="16H05">16H05-18H55</option>
+                                    <option value="19H05">19H05-21H55</option>
+                                </select>
+                                <select wire:model="salle4" name="salle4" id="salle4"class="form-control bg-soft-light border-light @error('salle4') is-invalid @enderror"  required >
+                                    <option value="">salle/capacité</option>
+                                    @foreach($salles4 as $salle)
+                                        <option value="{{ $salle->nom_salle }}">{{ $salle->nom_salle }}: {{ $salle->capacite }}</option>
+                                    @endforeach
+                                </select>
+                                <select wire:model="enseignant4" name="enseignant4" id="enseignant4"class="form-control bg-soft-light border-light @error('enseignant4') is-invalid @enderror"   required >
+                                    <option value="">enseignant</option>
+                                    @foreach($enseignants4 as $enseignant)
+                                        <option value="{{ $enseignant->nom_ens }}">{{ $enseignant->nom_ens }}</option>
+                                    @endforeach
+                                </select>
+                            
+                            @error('niveau')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
+                        </div>
+                    </div>
+                    @endif
                     <div>
                         <button class="btn btn-primary btn-block waves-effect waves-light" type="submit">{{ __("Créer emploi de temps") }}</button>
-                    </div>
-
-                    
-                    
+                    </div>                    
                 </form>
             </div>
         </div>
